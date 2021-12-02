ae6121b5d7fccca1e04ea8a2fa13394e6a0b1687
Markcino <markcinogbalazeh@gmai.com>
2021-12-02 21:48:36 +0000

    info activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -45,6 +45,12 @@ public class InfoActivity extends AppCompatActivity {
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
+    public void createPhoneIntent(View view){
+        Uri callUri = Uri.parse("tel:0123456789");
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(callUri);
+        startActivity(callIntent);
+    }
     // ***
 
 }
